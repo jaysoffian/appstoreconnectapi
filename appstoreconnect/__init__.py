8563cb03f6442fa6fb6bef9345e5de436ddbaba7
Jay Soffian <jsoffian@yahooinc.com>
2022-10-08 16:50:32 -0400

    Import all resources into appstoreconnect

--- appstoreconnect/__init__.py
+++ appstoreconnect/__init__.py
@@ -2,4 +2,27 @@
 appstoreconnect/__init__.py
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-from .api import Api, UserRole  # noqa
+from .api import Api  # noqa
+from .resources import (  # noqa
+    App,
+    AppEncryptionDeclaration,
+    BetaAppLocalization,
+    BetaAppReviewDetail,
+    BetaAppReviewSubmission,
+    BetaBuildLocalization,
+    BetaGroup,
+    BetaLicenseAgreement,
+    BetaTester,
+    Build,
+    BuildBetaDetail,
+    BundleId,
+    Certificate,
+    Device,
+    FinanceReport,
+    PreReleaseVersion,
+    Profile,
+    Resource,
+    SalesReport,
+    User,
+    UserInvitation,
+)
